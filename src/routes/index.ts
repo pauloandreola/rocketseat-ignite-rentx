72e6535389a0c64992a96a269af7ec9c4b00a452
Paulo Andreola <pauloandreola@gmail.com>
2022-11-01 16:44:21 -0300

    feat:Initial config on server.ts and routes index.ts file

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/prefer-default-export */
+import { Router } from "express";
+// import { authenticateRoutes } from "./authenticate.routes";
+// import { categoriesRoutes } from  "./categories.routes";
+// import { specificationsRoutes } from "./specifications.routes";
+// eslint-disable-next-line import/no-unresolved
+// import { usersRoutes } from "./users.routes";
+
+export const router = Router();
+
+// router.use(authenticateRoutes);
+// router.use("/cars", carsRoutes);
+// router.use("/cars_image", cars_imageRoutes);
+// router.use("/categories", categoriesRoutes);
+// router.use("/rentals", rentalsRoutes);
+// router.use("/specifications", specificationsRoutes);
+// router.use("/specifications_cars", specifications_carsRoutes);
+// router.use("/users", usersRoutes);
