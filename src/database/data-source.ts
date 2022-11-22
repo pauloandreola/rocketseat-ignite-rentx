087e1cb0dd26099389651f8afd01d9f466d1df19
Paulo Andreola <pauloandreola@gmail.com>
2022-11-21 23:51:35 -0300

    feat: database file

---
+++ src/database/data-source.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+import { User } from "../modules/accounts/entities/User";
+// import { Category } from "../modules/cars/entities/Category"
+// import { Specification } from "../modules/cars/entities/Specification"
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "teste",
+  port: 5432,
+  username: "docker",
+  password: "1234",
+  database: "rentx",
+  synchronize: true,
+  logging: true,
+  entities: [
+    // Category,
+    // Specification,
+    User,
+  ],
+  migrations: ["./src/database/migrations/*.ts"],
+});
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Initializing the database...");
+  })
+  .catch(err => console.log(err));
