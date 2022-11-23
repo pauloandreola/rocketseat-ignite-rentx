54481a87a123d202ed302cadab7a2e0744761e86
Paulo Andreola <pauloandreola@gmail.com>
2022-11-22 21:18:21 -0300

    fix: prettier config internal server error status created

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,14 @@ import swaggerUi = require('swagger-ui-express');
 
 // import './database';
 
-// import "./shared/container";
+import './shared/container';
 
+import { Request, Response } from 'express';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
-// import { NextFunction, Request, Response } from "express";
 import 'express-async-errors';
-// import { AppError } from "./errors/AppError";
+import { AppError } from './errors/appError';
+
 const app = express();
 const port = 3000;
 
@@ -21,17 +22,17 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-// app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-//   if(err instanceof AppError) {
-//     return response.status(err.statusCode).json({
-//       message: err.message
-//     })
-//   }
-//   return response.status(500).json({
-//     status: "error",
-//     message: `Internal server error - ${err.message}`,
-//   });
-// });
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(() => console.log(`Server is running port -> ${port}`));
 // console.log('Server is running port -> ', port);
