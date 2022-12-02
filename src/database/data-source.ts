23bac276ccd568b6744e3a2655174de75cf27fd6
Paulo Andreola <pauloandreola@gmail.com>
2022-12-01 21:46:56 -0300

    feat: adjust data-source file to migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,30 +1,35 @@
-// /* eslint-disable no-console */
-// import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
+/* eslint-disable no-console */
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 
-// import { User } from '../modules/accounts/entities/User';
-// // import { Category } from "../modules/cars/entities/Category"
-// // import { Specification } from "../modules/cars/entities/Specification"
+import { User } from '../modules/accounts/entities/User';
+// import { Category } from "../modules/cars/entities/Category"
+// import { Specification } from "../modules/cars/entities/Specification"
 
-// export const AppDataSource = new DataSource({
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: 'docker',
-//   password: 'ignite',
-//   database: 'rentx',
-//   synchronize: true,
-//   logging: true,
-//   entities: [
-//     // Category,
-//     // Specification,
-//     User,
-//   ],
-//   migrations: ['./src/database/migrations/*.ts'],
-// });
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'docker',
+  password: 'ignite',
+  database: 'rentx',
+  synchronize: true,
+  logging: true,
+  entities: [
+    // Category,
+    // Specification,
+    User,
+  ],
+  migrations: ['./src/database/migrations/*.ts'],
+  subscribers: [],
+});
 
-// AppDataSource.initialize()
-//   .then(async () => {
-//     console.log('Initializing the database...');
-//   })
-//   .catch(err => console.log(err));
+export function createConnection(host = 'database'): Promise<DataSource> {
+  return AppDataSource.setOptions({ host }).initialize();
+}
+
+AppDataSource.initialize()
+  .then(async () => {
+    console.log('Initializing the database...');
+  })
+  .catch(err => console.log(err));
