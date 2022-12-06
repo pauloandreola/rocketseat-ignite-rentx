14a52cb93bf41054028a991aed56e148e8adbefc
Paulo Andreola <pauloandreola@gmail.com>
2022-12-06 08:04:58 -0300

    fix: desestructure object in create user method

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -2,7 +2,14 @@ import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { User } from '../entities/User';
 
 export interface IUsersRepository {
-  create(data: ICreateUserDTO): Promise<void>;
+  create({
+    name,
+    email,
+    driverLicense,
+    password,
+    id,
+    avatar,
+  }: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
   findByName(name: string): Promise<User>;
