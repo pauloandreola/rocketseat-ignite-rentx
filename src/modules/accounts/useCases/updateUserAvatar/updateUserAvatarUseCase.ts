bd7b7c23411d0c658f321d121271fc93e0d0c14e
Paulo Andreola <pauloandreola@gmail.com>
2022-12-08 20:40:56 -0300

    refactor: usersrepository adjust

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -13,17 +13,17 @@ interface IRequest {
 @injectable()
 export class UpdateUserAvatarUseCase {
   constructor(
-    @inject('usersRepository') private UsersRepository: IUsersRepository,
+    @inject('usersRepository') private usersRepository: IUsersRepository,
   ) {}
 
   async execute({ userId, avatarFile }: IRequest): Promise<void> {
-    const user = await this.UsersRepository.findById(userId);
+    const user = await this.usersRepository.findById(userId);
 
     if (user.avatar) {
       await deleteFile(`./tmp/avatar/${user.avatar}`);
     }
 
     user.avatar = avatarFile;
-    await this.UsersRepository.create(user);
+    await this.usersRepository.create(user);
   }
 }
