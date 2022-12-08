60fe7adde22a758ec7e5366eccf17c0d859b2a8d
Paulo Andreola <pauloandreola@gmail.com>
2022-12-08 20:36:31 -0300

    refactor: import adjust sequence

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
-import express = require('express');
-import swaggerUi = require('swagger-ui-express');
-
-import './database/data-source';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 
 import './shared/container';
 
-import { Request, Response } from 'express';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
-import 'express-async-errors';
 import { AppError } from './errors/appError';
 
 const app = express();
@@ -22,17 +19,19 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use((err: Error, request: Request, response: Response) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      message: err.message,
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
     });
-  }
-  return response.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`,
-  });
-});
+  },
+);
 
 app.listen(() => console.log(`Server is running port -> ${port}`));
 // console.log('Server is running port -> ', port);
