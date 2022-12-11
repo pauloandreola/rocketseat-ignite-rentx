3c3a89570da1b256bc3b56bacc9e831af1bfd289
Paulo Andreola <pauloandreola@gmail.com>
2022-12-10 21:11:33 -0300

    feat: config categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,3 +1,10 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/createCategoryController';
 
 export const categoriesRoutes = Router();
+
+const createSpecificationController = new CreateCategoryController();
+
+categoriesRoutes.use(ensureAuthenticated);
+categoriesRoutes.post('/', createSpecificationController.handle);
