973d372371a86a5ed6e2c6df84e567f82171e393
Paulo Andreola <pauloandreola@gmail.com>
2022-12-10 21:10:56 -0300

    refactor: best practices change class and variable name specification routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
-import { CreateSpecificationsController } from '../modules/cars/useCases/createSpecification/createSpecificationController';
+import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/createSpecificationController';
 
 export const specificationsRoutes = Router();
 
-const createSpecificationsController = new CreateSpecificationsController();
+const createSpecificationController = new CreateSpecificationController();
 
 specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post('/', createSpecificationsController.handle);
+specificationsRoutes.post('/', createSpecificationController.handle);
