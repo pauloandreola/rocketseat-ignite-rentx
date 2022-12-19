1413c4f8fd225c86da05f8b0fba761b38635550d
Paulo Andreola <pauloandreola@gmail.com>
2022-12-19 18:09:47 -0300

    feat: seed admin and index files created but doesnt work

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+import { v4 as uuidV4 } from 'uuid';
+import { hash } from 'bcryptjs';
+
+import createConnection from '../index';
+
+async function create() {
+  const connection = createConnection('localhost');
+
+  const id = uuidV4();
+  const password = await hash('admin', 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license) values('${id}', 'admin', 'admin@tentx.com.br', '${password}', 'true', 'now()', 'XXXXX')`,
+  );
+  await connection.close;
+}
+
+create().then(() => console.log('User admin create'));
