849b774ce3b7643dd81b449dc036409716a3e883
Paulo Andreola <pauloandreola@gmail.com>
2022-12-19 18:03:29 -0300

    refactor: object data create adjust

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -2,13 +2,7 @@ import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { User } from '../infra/typeorm/entities/user';
 
 export interface IUsersRepository {
-  create({
-    name,
-    email,
-    driver_license,
-    password,
-  }: // avatar,
-  ICreateUserDTO): Promise<void>;
+  create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
   findByName(name: string): Promise<User>;
