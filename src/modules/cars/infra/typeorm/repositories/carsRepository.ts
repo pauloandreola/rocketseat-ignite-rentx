bc5fadefbdfe4d47620dbbd52ec078a29893178a
Paulo Andreola <pauloandreola@gmail.com>
2022-12-19 18:05:29 -0300

    feat: find available car in repository

--- src/modules/cars/infra/typeorm/repositories/carsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/carsRepository.ts
@@ -35,6 +35,29 @@ export class CarsRepository implements ICarsRepository {
     return car;
   }
 
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const carsQuery = await this.repository
+      .createQueryBuilder('c')
+      .where('available = :available', { available: true });
+
+    if (brand) {
+      carsQuery.andWhere('brand = :brand', { brand });
+    }
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', { category_id });
+    }
+    if (name) {
+      carsQuery.andWhere('name = :name', { name });
+    }
+    const cars = await carsQuery.getMany();
+
+    return cars;
+  }
+
   async findById(id: string): Promise<Car> {
     const car = await this.repository.findOneBy({ id });
     return car;
