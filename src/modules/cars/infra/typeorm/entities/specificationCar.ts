a4f7215daf861a3e7844ceecdd20181d76c3f9d4
Paulo Andreola <pauloandreola@gmail.com>
2022-12-21 19:45:50 -0300

    refactor: adjust columns

--- src/modules/cars/infra/typeorm/entities/specificationCar.ts
+++ src/modules/cars/infra/typeorm/entities/specificationCar.ts
@@ -1,13 +1,34 @@
-import { CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  PrimaryColumn,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
+import { Car } from './car';
+import { Specification } from './specification';
 
 @Entity('specificationsCar')
 export class SpecificationCar {
   @PrimaryColumn()
   id: string;
 
-  // car_id: string;
-  // specification_id: string;
+  @ManyToMany(() => Car)
+  @JoinColumn({ name: 'car_id' })
+  car: Car;
+
+  @Column()
+  car_id: string;
+
+  @ManyToMany(() => Specification)
+  @JoinColumn({ name: 'specification_id' })
+  specification: Specification;
+
+  @Column()
+  specification_id: string;
+
   @CreateDateColumn()
   created_at: Date;
 
