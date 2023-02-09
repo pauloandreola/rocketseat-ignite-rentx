4772715d46c615e8be20bf8442e3abb5bbd86f27
Paulo Andreola <pauloandreola@gmail.com>
2023-02-09 15:29:11 -0300

    fix: test method

--- src/modules/cars/useCases/createCar/createCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/createCarUseCase.spec.ts
@@ -26,18 +26,18 @@ describe('Create Car', () => {
   });
 
   it('Should not be able to create a new car with exists license plate', async () => {
-    expect(async () => {
-      await createCarUseCase.execute({
-        name: 'Car01',
-        description: 'Description car',
-        daily_rate: 100,
-        license_plate: 'ABC-1234',
-        fine_amount: 60,
-        brand: 'Brand',
-        category_id: 'Category',
-      });
+    await createCarUseCase.execute({
+      name: 'Car01',
+      description: 'Description car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category',
+    });
 
-      await createCarUseCase.execute({
+    await expect(
+      createCarUseCase.execute({
         name: 'Car02',
         description: 'Description car',
         daily_rate: 100,
@@ -45,8 +45,8 @@ describe('Create Car', () => {
         fine_amount: 60,
         brand: 'Brand',
         category_id: 'Category',
-      });
-    }).rejects.toBeInstanceOf(AppError);
+      }),
+    ).rejects.toEqual(new AppError('Car already exists!'));
   });
 
   it('Should not be able to create a car with available true by default', async () => {
--- src/modules/cars/useCases/createCategory/createCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/createCategoryUseCase.spec.ts
@@ -4,6 +4,7 @@ import { CreateCategoryUseCase } from './createCategoryUseCase';
 
 let createCategoryUseCase: CreateCategoryUseCase;
 let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
 describe('Create Category', () => {
   beforeEach(() => {
     categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
@@ -31,36 +32,21 @@ describe('Create Category', () => {
   });
 
   it('Should not be able to create a new category with name exists', async () => {
-    expect(async () => {
-      const category = {
-        name: 'Category Test',
-        description: 'Category description Test',
-      };
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test',
+    };
 
-      await createCategoryUseCase.execute({
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    await expect(
+      createCategoryUseCase.execute({
         name: category.name,
         description: category.description,
-      });
-
-      await createCategoryUseCase.execute({
-        name: category.name,
-        description: category.description,
-      });
-    }).rejects.toBeInstanceOf(AppError);
+      }),
+    ).rejects.toEqual(new AppError('Category already exists!'));
   });
 });
-
-// describe("Category create", () => {
-//   it("Expect 2 + 2 equal 4", () => {
-//     const soma = 2 + 2;
-//     const resultado = 4;
-
-//     expect(soma).toBe(resultado)
-//   });
-//   it("Expect 2 + 2 not to be 5", () => {
-//     const soma = 2 + 2;
-//     const resultado = 5;
-
-//     expect(soma).not.toBe(resultado);
-//   });
-// });
