dade0d3082245c67831e81b3b99c6fc927da2a58
Paulo Andreola <pauloandreola@gmail.com>
2023-02-10 07:46:02 -0300

    fix: update import file name

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -15,7 +15,7 @@ import { RentalsRepository } from '../../modules/rentals/infra/typeorm/repositor
 import { IRentalsRepository } from '../../modules/rentals/repositories/IRentalsRepository';
 import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/categoriesRepository';
 import { IUsersTokensRepository } from '../../modules/accounts/repositories/IUsersTokensRepository';
-import { UsersTokensRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import { UsersTokensRepository } from '../../modules/accounts/infra/typeorm/repositories/usersTokensRepository';
 
 container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
 
